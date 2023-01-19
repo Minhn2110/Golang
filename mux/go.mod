bc5e9f0a6c9f0d4d1f02235c7c6e9b479c746366
Minh <minhnha@smartosc.com>
2023-01-19 11:51:29 +0700

    feat: add mux

--- mux/go.mod
+++ mux/go.mod
@@ -3,3 +3,8 @@ module mux-tutorial
 go 1.19
 
 require github.com/gorilla/mux v1.8.0
+
+require (
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+)
--- mux/main.go
+++ mux/main.go
@@ -1,21 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
+	"github.com/go-resty/resty/v2"
 	"github.com/gorilla/mux"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Request:", r)
-	return
+
+	w.WriteHeader(http.StatusOK)
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		// logger.WithError(err).Error("failed to read request body")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid request body"})
+		return
+	}
+
+	fmt.Printf("%s", body)
+
+	client := resty.New()
+	resp, err := client.R().
+		EnableTrace().
+		SetHeaders(map[string]string{
+			"K-Trigger-Id":    "2Edk0ZEWS8c",
+			"K-Shared-Secret": "123456",
+		}).
+		SetBody(body).
+		Post("https://dev-api.kincloud.io/v1/webhook/kin-cloud-module-common/v1-0/2Edk0ZESXVD")
+
+	fmt.Printf("%s", resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Failed to start workflow"})
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Executed workflow success"})
+
 }
 
 func main() {
-
-	// fmt.Println("Response Info:")
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", r))
